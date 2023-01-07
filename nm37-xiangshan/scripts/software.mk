4a789a72f075b0dabb7cd2104baf0524bc8d618b
CHANG Yisong <changyisong@ict.ac.cn>
2023-01-07 11:25:40 +0000

    software: fix QDMA driver compilation
    support both host compilation and cross compilation

--- nm37-xiangshan/scripts/software.mk
+++ nm37-xiangshan/scripts/software.mk
@@ -1,4 +1,4 @@
-.PHONY: qdma_drv_armv8
+.PHONY: qdma_drv
 
 NM37_SW_LOC := target/nm37-xiangshan/software
 
@@ -8,20 +8,33 @@ QDMA_SRC := $(NM37_SW_LOC)/qdma/QDMA/linux-kernel
 QDMA_DRV_SRC := $(QDMA_SRC)/driver
 QDMA_APP_SRC := $(QDMA_SRC)/apps
 
-# ARMv8 kernel source files
-KERN_SRC_LOC := software/linux
+# ARMv8 kernel source files and cross-compiler PATH
+ifeq ($(TARGET_HOST),aarch64)
+KSRC := $(abspath software/linux)
+KOBJ:= $(abspath software/arm-linux/phy_os)
+endif
+
+EXPORT_CC_PATH := export PATH=$(LINUX_GCC_PATH):$$PATH
+
+ifeq ($(TARGET_HOST),aarch64)
+drv-flags := ARCH=$(TARGET_HOST) CROSS_COMPILE=$(LINUX_GCC_PREFIX) \
+             KSRC=$(KSRC) KOBJ=$(KOBJ)
+else
+drv-flags := 
+endif
 
 # generated Linux symbol version file
 modulesymfile := $(abspath $(NM37_SW_LOC)/build/qdma/drv/Module.symvers)
 
-qdma_drv_armv8: 
+qdma_drv:
 	@echo "Compiling QDMA Linux driver..."
 	@mkdir -p $(NM37_SW_LOC)/build/qdma/drv 
 	$(EXPORT_CC_PATH) && $(MAKE) $(MODULE) -C $(QDMA_DRV_SRC) \
-		modulesymfile=$(modulesymfile) \
-		CROSS_COMPILE=$(CROSS_COMPILE)
-		KDIR=$(KERN_SRC_LOC)
+		modulesymfile=$(modulesymfile) $(drv-flags)
 
-qdma_drv_armv8_clean:
+	@mv $(abspath $(QDMA_DRV_SRC)/../bin/*) $(abspath $(NM37_SW_LOC)/build/qdma/drv)
+
+qdma_drv_clean:
 	$(MAKE) -C $(QDMA_DRV_SRC) clean
+	@rm -rf $(NM37_SW_LOC)/build/qdma/drv
 
