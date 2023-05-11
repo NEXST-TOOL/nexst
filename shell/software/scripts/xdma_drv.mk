16d303f83ab8a3f6d0496e9b0be5459b208048a6
CHANG Yisong <changyisong@ict.ac.cn>
2023-05-11 09:10:26 +0000

    shell: add compilation of host-side PCIe driver
    The PCIe driver is common for various target FPGA boards.
    
    We would like to fix cross compilation for aarch64 platform soon

---
+++ shell/software/scripts/xdma_drv.mk
@@ -0,0 +1,40 @@
+#=================================================
+# Parameters for XDMA Driver
+#=================================================
+XDMA_SRC := shell/software/xdma_drv/XDMA/linux-kernel
+
+# source file locations of XDMA driver and apps
+XDMA_DRV_SRC := $(XDMA_SRC)/xdma
+XDMA_TOOL_SRC := $(XDMA_SRC)/tools
+
+# ARMv8 kernel source files and cross-compiler PATH
+ifeq ($(TARGET_HOST),aarch64)
+KSRC := $(abspath software/linux)
+KOBJ:= $(abspath software/arm-linux/phy_os)
+endif
+
+EXPORT_CC_PATH := export PATH=$(LINUX_GCC_PATH):$$PATH
+
+ifeq ($(TARGET_HOST),aarch64)
+drv-flags := ARCH=$(TARGET_HOST) CROSS_COMPILE=$(LINUX_GCC_PREFIX) \
+             KSRC=$(KSRC) KOBJ=$(KOBJ)
+else
+drv-flags := 
+endif
+
+GEN_TARGET := *.ko modules.order *.mod.c *.o .*.o.cmd *.mod .*.ko.cmd .*.mod.cmd .*.order.cmd .*.symvers.cmd Module.symvers
+GEN_FILES := $(foreach obj,$(GEN_TARGET),$(XDMA_DRV_SRC)/$(obj)) 
+
+#=================================================
+# XDMA Driver Compilation
+#=================================================
+xdma_drv:
+	@echo "Compiling XDMA Linux driver..."
+	@mkdir -p shell/software/build/xdma_drv
+	$(EXPORT_CC_PATH) && $(MAKE) $(MODULE) -C $(XDMA_DRV_SRC) \
+		$(drv-flags)
+	@mv $(GEN_FILES) $(abspath shell/software/build/xdma_drv)
+
+xdma_drv_clean:
+	$(MAKE) -C $(XDMA_DRV_SRC) clean
+	@rm -rf shell/software/build/xdma_drv
