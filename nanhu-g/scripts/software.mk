8218a615cefa81fbc5715cdde832f70dac1c893b
CHANG Yisong <changyisong@ict.ac.cn>
2023-10-16 11:33:57 +0000

    nanhu-g: software: fix parameter passing in dt and opensbi compilation
    Users are able to specify `HART_COUNT` and `DT_TARGET` in the MAKE CLI

--- nanhu-g/scripts/software.mk
+++ nanhu-g/scripts/software.mk
@@ -6,6 +6,8 @@ EXPORT_CC_PATH := export PATH=$(LINUX_GCC_PATH):$$PATH
 
 EXPORT_DTC_PATH := export PATH=$(DTC_LOC):$$PATH
 
+HART_COUNT ?= 1
+
 include $(NANHU_G_SW_LOC)/../scripts/sw_target/opensbi.mk
 
 include $(NANHU_G_SW_LOC)/../scripts/sw_target/kernel.mk
