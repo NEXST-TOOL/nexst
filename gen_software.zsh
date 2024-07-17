dac828fa6f37d3059c4ce0d28a569d47135bba91
Gwins7 <Gwins777@qq.com>
2024-07-17 12:42:49 +0800

    DMA: update xiangshan_dma.tcl; modify dts

--- gen_software.zsh
+++ gen_software.zsh
@@ -1,8 +1,9 @@
-export RISCV=/data/hty/old-riscv-gcc
+export RISCV=/data/hty/tools/old-riscv-gcc
 export PATH=$RISCV/bin:$PATH
+
 cd work_farm
 make PRJ="target:nanhu-g:proto" FPGA_BD=vcu128 ARCH=riscv zsbl
-make PRJ="target:nanhu-g:proto" FPGA_BD=vcu128 DT_TARGET=XSTop_pci target_dt
+make PRJ="target:nanhu-g:proto" FPGA_BD=vcu128 DT_TARGET=XSTop target_dt
 make PRJ="target:nanhu-g:proto" FPGA_BD=vcu128 ARCH=riscv phy_os.os
-make PRJ="target:nanhu-g:proto" FPGA_BD=vcu128 ARCH=riscv DT_TARGET=XSTop_pci opensbi
+make PRJ="target:nanhu-g:proto" FPGA_BD=vcu128 ARCH=riscv DT_TARGET=XSTop opensbi
 cd ..
