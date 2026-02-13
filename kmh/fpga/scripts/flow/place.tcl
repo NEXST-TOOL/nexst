9fc4923133e4522ab9e551ba49ca88a100d03bb3
Liu Shiqi <liushiqi17@mails.ucas.ac.cn>
2026-02-13 17:55:17 +0800

    Update nanhu-g flow scripts for UVHS-2

--- kmh/fpga/scripts/flow/place.tcl
+++ kmh/fpga/scripts/flow/place.tcl
@@ -1,8 +1,8 @@
 # open checkpoint of the opt_design phase
 # open_checkpoint ${dcp_dir}/opt_pass_3.dcp
 
-# Placement with SSI logic spreading strategy for congestion avoidance 
-place_design -directive SSI_SpreadLogic_high
+# Placement
+place_design
 
 report_utilization -file ${impl_rpt_dir}/post_place_util.rpt
 report_timing_summary -file ${impl_rpt_dir}/post_place_timing_setup.rpt -delay_type max -max_paths 1000
@@ -28,7 +28,7 @@ report_timing_summary -file ${impl_rpt_dir}/post_place_phys_opt_aggressive_fanou
 
 write_checkpoint -force ${dcp_dir}/post_place_phys_opt_aggressive_fanout_opt.dcp
 
-## Pass 3: Retiming 
+## Pass 3: Retiming
 phys_opt_design -directive AddRetime
 
 report_utilization -file ${impl_rpt_dir}/post_place_phys_opt_retiming_util.rpt
@@ -52,5 +52,5 @@ phys_opt_design -directive ExploreWithAggressiveHoldFix
 report_utilization -file ${impl_rpt_dir}/post_place_phys_opt_aggressive_hold_fix_util.rpt
 report_timing_summary -file ${impl_rpt_dir}/post_place_phys_opt_aggressive_hold_fix_timing_setup.rpt -delay_type max -max_paths 1000
 report_timing_summary -file ${impl_rpt_dir}/post_place_phys_opt_aggressive_hold_fix_timing_hold.rpt -delay_type min -max_paths 1000
-		
+
 write_checkpoint -force ${dcp_dir}/post_place_phys_opt_aggressive_hold_fix.dcp
